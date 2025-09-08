df450c7c050ffe688827a58e12fe8707fe8ba5ef
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-09-08 10:46:19 +0800

    added a file

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -16,3 +16,14 @@ FROM (
     JOIN booking b ON p.property_id = b.property_id
     GROUP BY p.property_id, b.status
 ) AS sub;
+
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_num
+FROM property p
+LEFT JOIN booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY total_bookings DESC;
+
